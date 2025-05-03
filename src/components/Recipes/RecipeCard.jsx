7e9115fc6363f786c655338f0b2645bf4f0eb23d
celinedrules <celinedrules@hotmail.com>
2025-05-03 11:39:27 -0600

    Changed a tag to Link

--- src/components/Recipes/RecipeCard.jsx
+++ src/components/Recipes/RecipeCard.jsx
@@ -1,16 +1,17 @@
 ﻿import "./Recipes.scss";
+import {Link} from "react-router-dom";
 
 const RecipeCard = ({recipe}) => {
     return (
         <div className="card">
-            <a href={`/recipe/${recipe.id}`}>
+            <Link to={`/recipe/${recipe.id}`}>
                 <div>
                     <img src={recipe.image_url} alt={recipe.name}/>
                 </div>
                 <div className="title">
                     {recipe.name}
                 </div>
-            </a>
+            </Link>
         </div>
     );
 };
